b9beff42a1f49ed040de90d74cae7cfd03036505
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-04-24 15:58:36 +0300

    Update lava.tsx

--- src/constants/services/lava.tsx
+++ src/constants/services/lava.tsx
@@ -26,29 +26,6 @@ export const Installation: ContentType[] = [
   },
 ];
 
-export const Sidecar: ContentType[] = [
-  {
-    title: "Install Go",
-    method: "",
-    code: `sudo rm -rvf /usr/local/go/
-     wget https://golang.org/dl/go1.22.4.linux-amd64.tar.gz
-     sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
-     rm go1.22.4.linux-amd64.tar.gz`,
-  },
-  {
-    title: "Configure Go",
-    method: "",
-    code: `export GOROOT=/usr/local/go
-     export GOPATH=$HOME/go
-     export GO111MODULE=on
-     export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin`,
-  },
-  {
-    title: "Install Cosmovisor",
-    method: "",
-    code: `go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0`,
-  },
-];
 export const Endpoints: ContentType[] = [
   {
     title: "Install Go",
