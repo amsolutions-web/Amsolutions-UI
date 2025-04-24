d1a582ac9f52bec109d38c115e338937f01b5641
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-04-24 15:26:30 +0300

    Create lava.tsx

---
+++ src/pages/services/lava.tsx
@@ -0,0 +1,186 @@
+import {
+  Command,
+  Endpoints,
+  Installation,
+  Peer,
+  Scanner,
+  Sidecar,
+  Snapshot,
+  Statesync,
+} from "@/constants/services/airchains";
+import { Box, Stack } from "@mui/material";
+import { useRouter } from "next/router";
+import SidebarLayout from "@/sections/layouts";
+import { useMemo } from "react";
+import { ContentSection } from "@/components/content-section";
+import { StyledTitle } from "@/components/styled-title";
+
+const sidebarWidth = 240;
+
+const Airchains = () => {
+  const router = useRouter();
+  const pathName = useMemo(
+    () => router.pathname.split("/").pop(),
+    [router.pathname]
+  );
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      flexDirection={"column"}
+      alignItems={"center"}
+      sx={{
+        ml: { sm: `${sidebarWidth}px` },
+        width: { sm: `calc(100% - ${sidebarWidth}px)` },
+        pt: { sm: "150px", xs: "30px" },
+        gap: 5,
+      }}
+    >
+      <div id="install">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">
+            {pathName} Node Installation Guide
+          </StyledTitle>
+
+          {/* Content Mapping */}
+          {Installation.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="sidecar">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">Install Sidecar</StyledTitle>
+
+          {/* Content Mapping */}
+          {Sidecar.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="endpoints">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4"> RPC, API, GRPC Endpoints</StyledTitle>
+
+          {/* Content Mapping */}
+          {Endpoints.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="statesync">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">Statesync</StyledTitle>
+
+          {/* Content Mapping */}
+          {Statesync.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="snapshot">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">Snapshot</StyledTitle>
+
+          {/* Content Mapping */}
+          {Snapshot.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="peer">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">Peer,seed, addrbook</StyledTitle>
+
+          {/* Content Mapping */}
+          {Peer.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="command">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">CLI commands</StyledTitle>
+
+          {/* Content Mapping */}
+          {Command.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+      <div id="scanner">
+        <Stack
+          pb="10px"
+          sx={{
+            px: { xs: "10px" },
+            maxWidth: "800px",
+          }}
+        >
+          {/* Header */}
+          <StyledTitle variant="h4">RPC Scanner</StyledTitle>
+
+          {/* Content Mapping */}
+          {Scanner.map((item, index) => (
+            <ContentSection key={index} item={item} />
+          ))}
+        </Stack>
+      </div>
+    </Box>
+  );
+};
+
+Airchains.getLayout = function getLayout(page: React.ReactElement) {
+  return <SidebarLayout>{page}</SidebarLayout>;
+};
+
+export default Airchains;
