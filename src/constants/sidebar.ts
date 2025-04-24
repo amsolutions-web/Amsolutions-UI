9baa5bcfbb92e386779ddd02857b519d03182566
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-04-24 15:57:26 +0300

    Update sidebar.ts

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,6 +1,5 @@
 export const SidebarItems = [
     {label: 'installation', target:'install'},
-    {label: 'sidecar', target:'sidecar'},
     {label: 'rpc, api, grpc', target:'endpoints'},
     {label: 'statesync', target:'statesync'},
     {label: 'snapshot', target:'snapshot'},
