a34efce736f79d736b0275d9328a621a750efe20
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-14 15:36:44 +0300

    Update sidebar.ts

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,7 +1,7 @@
 export const SidebarItems = [
     {label: 'installation', target:'install'},
     {label: 'endpoints', target:'endpoints'},
-    {label: 'statesync', target:'statesync'},
+    {label: 'statesync', target:'blabla'},
     {label: 'snapshot', target:'snapshot'},
     {label: 'peers, seed', target:'peer'},
     {label: 'useful commands', target:'command'},
