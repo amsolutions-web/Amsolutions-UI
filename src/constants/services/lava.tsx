3bfe91cf5a774db35987c90855c6aaa0a61dfff5
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-15 14:34:33 +0300

    Update lava.tsx

--- src/constants/services/lava.tsx
+++ src/constants/services/lava.tsx
@@ -2,13 +2,12 @@ import { ContentType } from "@/types";
 
 export const Installation: ContentType[] = [
   {
-    title: "Install Go",
+    title: "Essential tools",
     method:
-      "We will use Go v1.22.4 as example here. The code below also cleanly removes any previous Go installation.",
-    code: `sudo rm -rvf /usr/local/go/
-     wget https://golang.org/dl/go1.22.4.linux-amd64.tar.gz
-     sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
-     rm go1.22.4.linux-amd64.tar.gz`,
+      "Essential tools for building.",
+    code: `sudo apt -y update
+sudo apt -y install curl git jq lz4 build-essential
+sudo apt -y upgrade`,
   },
   {
     title: "Install Cosmovisor",
