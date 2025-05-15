cac657c69dce048e79259a354183966296654237
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-15 13:50:31 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['itrocket.net'],
+    domains: ['theamsolutions.info'],
   },
 };
 
