a201a5b22a7d3aeeedc8f9505a882bc317a3b223
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-16 11:30:49 +0300

    Update lava.tsx

--- src/constants/services/lava.tsx
+++ src/constants/services/lava.tsx
@@ -110,7 +110,8 @@ export const Endpoints: ContentType[] = [
   {
     title: "Launch node VIA Cosmovisor",
     method: "Install Cosmovisor",
-    code: `go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0`,
+    code: `go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0
+    `,
   },
   {
     title: "",
