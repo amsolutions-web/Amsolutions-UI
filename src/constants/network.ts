4c478641b51df556069d6ec24e999ad37611eb4d
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-29 15:47:59 +0300

    Update network.ts

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -4,10 +4,10 @@ export enum Network {
 }
 
 export const Mainnet: Array<{ alt: string; url: string; title: string; id: string }> = [
-    { alt:'LAVA', url: "https://avatars.githubusercontent.com/u/100386277?s=48&v=4", title: "lava", id:'API INFRA'},
-    { alt:'Archway', url: "https://raw.githubusercontent.com/cosmos/chain-registry/master/archway/images/archway.png", title: "archway", id:'L1 for smart contracts'},
-    { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
-    { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
+    { alt:'LAVA', url: "https://avatars.githubusercontent.com/u/100386277?s=48&v=4", title: "lava", id:''},
+    { alt:'Archway', url: "https://raw.githubusercontent.com/cosmos/chain-registry/master/archway/images/archway.png", title: "archway", id:''},
+    { alt:'Swisstronik', url: "https://cdn-images-1.medium.com/max/184/1*LOYzqNQCdg11pkS-f1TbvQ@2x.jpeg", title: "swisstronik", id:''},
+    { alt:'Penumbra', url: "https://avatars.githubusercontent.com/u/85261160?s=200&v=4", title: "penumbra", id:''},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
