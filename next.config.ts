8f27ea0abef637c3bf3e7a3d01692c15a14a0df1
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-29 15:39:23 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['itrocket.net'],
+    domains: ['raw.githubusercontent.com','avatars.githubusercontent.com'],
   },
 };
 
