8e35103b42bbbba31739ec47dcd98d7cdf5e901f
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-30 15:11:42 +0300

    Update network.ts

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -6,9 +6,9 @@ export enum Network {
 export const Mainnet: Array<{ alt: string; url: string; title: string; id: string }> = [
     { alt:'LAVA', url: "https://avatars.githubusercontent.com/u/100386277?s=48&v=4", title: "lava", id:''},
     { alt:'Archway', url: "https://raw.githubusercontent.com/cosmos/chain-registry/master/archway/images/archway.png", title: "archway", id:''},
-    { alt:'Swisstronik', url: "https://brandfetch.com/swisstronik.com?view=library&library=default&collection=logos&asset=id_jCRjlcR", title: "swisstronik", id:''},
+    { alt:'Swisstronik', url: "https://assets.brandfetch.io/brand/logo/Swisstronik.svg", title: "swisstronik", id:''},
     { alt:'Penumbra', url: "https://avatars.githubusercontent.com/u/85261160?s=200&v=4", title: "penumbra", id:''},
-    { alt:'Side Protocol', url: "https://avatars.githubusercontent.com/u/111408987?s=48&v=4", title: "side", id:''},
+    { alt:'Side Protocol', url: "https://avatars.githubusercontent.com/u/111408987?s=48&v=4", title: "side protocol", id:''},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
