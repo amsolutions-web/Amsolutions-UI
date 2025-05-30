f512939aae42e4c65e9310d76401130f213061bc
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-30 15:14:21 +0300

    Update network.ts

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -6,7 +6,7 @@ export enum Network {
 export const Mainnet: Array<{ alt: string; url: string; title: string; id: string }> = [
     { alt:'LAVA', url: "https://avatars.githubusercontent.com/u/100386277?s=48&v=4", title: "lava", id:''},
     { alt:'Archway', url: "https://raw.githubusercontent.com/cosmos/chain-registry/master/archway/images/archway.png", title: "archway", id:''},
-    { alt:'Swisstronik', url: "https://assets.brandfetch.io/brand/logo/Swisstronik.svg", title: "swisstronik", id:''},
+    { alt:'Swisstronik', url: "https://itrocket.net/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Fswisstronic.aeb7c18c.jpg&w=96&q=75", title: "swisstronik", id:''},
     { alt:'Penumbra', url: "https://avatars.githubusercontent.com/u/85261160?s=200&v=4", title: "penumbra", id:''},
     { alt:'Side Protocol', url: "https://avatars.githubusercontent.com/u/111408987?s=48&v=4", title: "side protocol", id:''},
     { alt:'airchains', url: "https://itrocket.net/_next/image/?url=%2Ftestnet%2Fairchains.jpg&w=96&q=75", title: "airchains", id:'junction'},
