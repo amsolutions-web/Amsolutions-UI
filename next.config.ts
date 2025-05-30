301a8a7283293154548f631e38c9d9b98497f9af
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2025-05-30 15:14:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: true,,
   images: {
-    domains: ['raw.githubusercontent.com','avatars.githubusercontent.com','assets.brandfetch.io'],
+    domains: ['raw.githubusercontent.com','avatars.githubusercontent.com','itrocket.net',],
   },
 };
 
