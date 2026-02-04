d476df61c5c1f69fa7e308fbc08c2bc60b013c55
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-04 14:42:25 +0200

    Update network.ts

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -8,7 +8,7 @@ export const Mainnet: Array<{ alt: string; url: string; title: string; id: strin
     { alt:'Archway', url: "https://raw.githubusercontent.com/cosmos/chain-registry/master/archway/images/archway.png", title: "archway", id:''},
     { alt:'Swisstronik', url: "https://itrocket.net/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Fswisstronic.aeb7c18c.jpg&w=96&q=75", title: "swisstronik", id:''},
     { alt:'Penumbra', url: "https://avatars.githubusercontent.com/u/85261160?s=200&v=4", title: "penumbra", id:''},
-    { alt:'Side Protocol', url: "https://avatars.githubusercontent.com/u/111408987?s=48&v=4", title: "side protocol", id:''},
+    { alt:'Bitway', url: "https://avatars.githubusercontent.com/u/223204087?s=200&v=4", title: "side protocol", id:''},
     { alt:'C4E', url: "https://avatars.githubusercontent.com/u/101329004?s=48&v=4", title: "c4e", id:'c4e'},
     { alt:'Zenrock', url: "https://avatars.githubusercontent.com/u/160767775?s=48&v=4", title: "zenrock", id:''},
     { alt:'UX', url: "https://avatars.githubusercontent.com/u/79238191?s=48&v=4", title: "ux", id:''},
