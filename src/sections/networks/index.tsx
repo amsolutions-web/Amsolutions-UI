ccd6d23fa10b95c750b92311bb6dee5ea4293004
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-05 21:06:26 +0200

    Update index.tsx

--- src/sections/networks/index.tsx
+++ src/sections/networks/index.tsx
@@ -6,72 +6,77 @@ const NetworksSection = () => {
   return (
     <div id="networks">
       <Box
-        flexDirection={"column"}
+        display="flex"
+        flexDirection="column"
         sx={{ px: { sm: "80px", xs: "20px" }, py: "20px" }}
-        bgcolor={"#f1f1f1"}
+        bgcolor="#f1f1f1"
       >
-        <Stack flexDirection={"column"} justifyContent={"center"} gap={4}>
+        {/* MAINNET */}
+        <Stack flexDirection="column" justifyContent="center" gap={4}>
           <Typography
             variant="h4"
-            fontWeight={"bold"}
-            textTransform={"uppercase"}
-            textAlign={"center"}
+            fontWeight="bold"
+            textTransform="uppercase"
+            textAlign="center"
           >
             featured projects
           </Typography>
+
           <Stack
             gap={3}
-            px={"30px"}
-            bgcolor={"#fdfdfd"}
-            flexDirection={"row"}
+            px="30px"
+            bgcolor="#fdfdfd"
+            flexDirection="row"
             py={3}
-            justifyContent={"center"}
-            flexWrap={"wrap"}
+            justifyContent="center"
+            flexWrap="wrap"
           >
-{Mainnet.map((item, index) => (
-  <NetworkCard
-    key={index}
-    alt={item.alt}
-    url={item.url}
-    title={item.title}
-    id={item.id}
-    stakingUrl={item.stakingUrl}
-    serviceUrl={item.serviceUrl}
-  />
-))}
-
+            {Mainnet.map((item) => (
+              <NetworkCard
+                key={item.title}
+                alt={item.alt}
+                url={item.url}
+                title={item.title}
+                id={item.id}
+                stakingUrl={item.stakingUrl}
+                serviceUrl={item.serviceUrl}
+              />
+            ))}
           </Stack>
         </Stack>
-        <Stack flexDirection={"column"} justifyContent={"center"}>
+
+        {/* TESTNET */}
+        <Stack flexDirection="column" justifyContent="center">
           <Typography
             variant="h4"
-            fontWeight={"bold"}
-            textTransform={"uppercase"}
-            textAlign={"center"}
+            fontWeight="bold"
+            textTransform="uppercase"
+            textAlign="center"
             my={4}
           >
             ongoing testnets
           </Typography>
+
           <Stack
             gap={3}
-            px={"30px"}
-            bgcolor={"#fdfdfd"}
-            flexDirection={"row"}
+            px="30px"
+            bgcolor="#fdfdfd"
+            flexDirection="row"
             py={3}
-            justifyContent={"center"}
-            flexWrap={"wrap"}
+            justifyContent="center"
+            flexWrap="wrap"
           >
-{Testnet.map((item, index) => (
-  <NetworkCard
-    key={index}
-    alt={item.alt}
-    url={item.url}
-    title={item.title}
-    id={item.id}
-    stakingUrl={item.stakingUrl}
-    serviceUrl={item.serviceUrl}
-  />
-))}
+            {Testnet.map((item) => (
+              <NetworkCard
+                key={item.title}
+                alt={item.alt}
+                url={item.url}
+                title={item.title}
+                id={item.id}
+                stakingUrl={item.stakingUrl}
+                serviceUrl={item.serviceUrl}
+              />
+            ))}
           </Stack>
         </Stack>
       </Box>
