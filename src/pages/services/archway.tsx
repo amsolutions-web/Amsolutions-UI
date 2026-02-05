7f938fea99f260da10074d607ade17265177d07b
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-05 15:20:58 +0200

    Update archway.tsx

--- src/pages/services/archway.tsx
+++ src/pages/services/archway.tsx
@@ -1,15 +1,14 @@
-import { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination:
-        "https://docs.archway.io/validators/running-a-node/install",
-      permanent: false,
-    },
-  };
-};
+import { useEffect } from "react";
 
 export default function ArchwayRedirect() {
+  useEffect(() => {
+    window.open(
+      "https://docs.archway.io/validators/running-a-node/install",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  }, []);
+
   return null;
 }
+
