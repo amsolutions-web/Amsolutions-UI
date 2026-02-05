01f6cbf0b6a77803cf57adb4e21bfe90af76cdf6
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-05 15:29:33 +0200

    Update archway.tsx

--- src/pages/services/archway.tsx
+++ src/pages/services/archway.tsx
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function ArchwayRedirect() {
+  const router = useRouter();
+
   useEffect(() => {
     window.open(
       "https://docs.archway.io/validators/running-a-node/install",
       "_blank",
       "noopener,noreferrer"
     );
-  }, []);
+    router.replace("/");
+  }, [router]);
 
   return null;
 }
-
