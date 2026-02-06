be21c3487804e174b56806c8ba449b252a91e290
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-06 15:43:30 +0200

    Update network.ts

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -4,12 +4,22 @@ export enum Network {
 }
 
 export const Mainnet: Array<{ alt: string; url: string; title: string; id: string, stakingUrl?: string, serviceUrl?: string }> = [
+    { alt:'Warden Protocol', url: "https://avatars.githubusercontent.com/u/158038121?s=48&v=4", title: "warden", id:'', 
+    serviceUrl: "https://docs.wardenprotocol.org", 
+    stakingUrl: "https://app.wardenprotocol.org/earn" },
+
+    { alt:'Bitway', url: "https://avatars.githubusercontent.com/u/223204087?s=200&v=4", title: "Bitway", id:'',
+    serviceUrl: "https://docs.bitway.com", 
+    stakingUrl: "https://ping.pub/bitway/staking/bitwayvaloper1qqrp7yggqv8qxpcgqy0syqc0ruy3jyg5r50qq9gnzcqq5rcwzs23zsdx4hq"},
+    
     { alt:'LAVA', url: "https://avatars.githubusercontent.com/u/100386277?s=48&v=4", title: "lava", id:'',
      stakingUrl:"https://lavacenter.xyz/lava/staking/lava@valoper179mdmkdejgj0fk68gjvpknm9dv94tmu0zyqmjx"},
     
     { alt:'Archway', url: "https://raw.githubusercontent.com/cosmos/chain-registry/master/archway/images/archway.png", title: "archway", id:'',
      stakingUrl:"https://www.mintscan.io/archway/validators/archwayvaloper18wdwndqaaukkgwn4s0ynw7mdddty6h6vtm2g8p",
     serviceUrl: "https://docs.archway.io/validators/running-a-node/install"},
+
+    { alt:'Zenrock', url: "https://avatars.githubusercontent.com/u/160767775?s=48&v=4", title: "zenrock", id:''},
     
     { alt:'Swisstronik', url: "https://itrocket.net/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Fswisstronic.aeb7c18c.jpg&w=96&q=75", title: "swisstronik", id:'',
      serviceUrl:"https://docs.swisstronik.com/swisstronik-docs",
@@ -19,14 +29,11 @@ export const Mainnet: Array<{ alt: string; url: string; title: string; id: strin
     serviceUrl: "https://guide.penumbra.zone", 
     stakingUrl: "https://explorer.penumbra.zone/validator/penumbravalid1z6nw9ute35ajjpvq5yqhspvvvp5ephyd8v5c9wk7hs2zz465esrq56r40w"},
     
-    { alt:'Bitway', url: "https://avatars.githubusercontent.com/u/223204087?s=200&v=4", title: "Bitway", id:'',
-    serviceUrl: "https://docs.bitway.com", 
-    stakingUrl: "https://ping.pub/bitway/staking/bitwayvaloper1qqrp7yggqv8qxpcgqy0syqc0ruy3jyg5r50qq9gnzcqq5rcwzs23zsdx4hq"},
     { alt:'C4E', url: "https://avatars.githubusercontent.com/u/101329004?s=48&v=4", title: "c4e", id:'c4e'},
-    { alt:'Zenrock', url: "https://avatars.githubusercontent.com/u/160767775?s=48&v=4", title: "zenrock", id:''},
+    
     { alt:'UX', url: "https://avatars.githubusercontent.com/u/79238191?s=48&v=4", title: "ux", id:''},
     { alt:'Quicksilver', url: "https://avatars.githubusercontent.com/u/144680245?s=48&v=4", title: "quicksilver", id:''},
-    { alt:'Warden Protocol', url: "https://avatars.githubusercontent.com/u/158038121?s=48&v=4", title: "warden", id:''},
+    
 ]
 
 export const Testnet: Array<{ alt: string; url: string; title: string; id: string, stakingUrl?: string, serviceUrl?: string }> = [
