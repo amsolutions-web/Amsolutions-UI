4ffba18882fcdf5833f2931b5ea0aea6fd76cd55
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-11 14:29:28 +0200

    Update social-icons.tsx

--- src/constants/social-icons.tsx
+++ src/constants/social-icons.tsx
@@ -1,11 +1,34 @@
+import { IconType } from "react-icons";
 import { AiFillDiscord } from "react-icons/ai";
 import { PiXLogoFill } from "react-icons/pi";
 import { ImMail4 } from "react-icons/im";
 import { FaTelegramPlane } from "react-icons/fa";
 
-export const SocialIcons = [
-  AiFillDiscord,
-  PiXLogoFill,
-  ImMail4,
-  FaTelegramPlane,
+export type SocialIcon = {
+  icon: IconType;
+  href: string;
+  label: string;
+};
+
+export const SocialIcons: SocialIcon[] = [
+  {
+    icon: AiFillDiscord,
+    href: "https://discord.com/amsolutions",
+    label: "Discord",
+  },
+  {
+    icon: PiXLogoFill,
+    href: "https://x.com/Alexey48110005",
+    label: "X (Twitter)",
+  },
+  {
+    icon: ImMail4,
+    href: "latflat@gmail.com",
+    label: "Email",
+  },
+  {
+    icon: FaTelegramPlane,
+    href: "https://t.me/AlekseyRiga",
+    label: "Telegram",
+  },
 ];
