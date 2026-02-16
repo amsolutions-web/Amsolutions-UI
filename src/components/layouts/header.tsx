dce3c2a0f05494bf175aac378c679e65e61a4e13
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2026-02-16 15:11:22 +0200

    Update header.tsx

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -84,8 +84,8 @@ const Header = (props: Props) => {
       zIndex: -1,
 
       animation: `
-        zoomMotion 40s ease-in-out infinite alternate,
-        blurMotion 20s ease-in-out infinite alternate
+        zoomMotion 6s ease-in-out infinite alternate,
+        blurMotion 6s ease-in-out infinite alternate
       `,
     },
   }}
